dd0473b0b815cd911e9a138428b568be4e685397
Graham Miller <graham-miller@users.noreply.github.com>
2017-10-06 16:02:43 +0100

    Revise the HeroDetailComponent
    Trying to get Revise the HeroDetailComponent to work (https://angular.io/tutorial/toh-pt5#add-heroservicegethero)

--- angular-tour-of-heroes/src/app/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from '@angular/core';
+// Keep the Input import for now, you'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
 
+import 'rxjs/add/operator/switchMap';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
+
 @Component({
   selector: 'hero-detail',
   template: `
     <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
       <div>
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
@@ -14,6 +23,18 @@ import { Hero } from './hero';
     </div>
   `
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
   @Input() hero: Hero;
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+      .subscribe(hero => this.hero = hero);
+  }
 }
